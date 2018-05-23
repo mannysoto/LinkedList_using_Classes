b23392b58d88fa7640cb484d09d4da4bbf9d5049
Manny Soto <mannysoto@yahoo.com>
2018-05-22 21:46:33 -0600

    clear objects from heap

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ int main()
     int intSentinel = 0;
     int tmpCounter = 0;
     Node *current = NULL, *previous = NULL, *next = NULL, *head = NULL, *tail = NULL;
+    char charDeleteNode = 'a';
 
     //Ensure intSentinel does not equal 0, otherwise, the user won't go into the for loop.
     intSentinel = (intSentinel == 0) ? 1 : 0;
@@ -44,13 +45,50 @@ int main()
     };
 
     current = head;
+    previous = NULL;
+    while(current != NULL){
+        cout << "Current: " << current << endl;
+        cout << "\tData: " << current->intData << endl;
+        cout << "\tNext Node: " << current->nextNode << endl;
+        cout << "\t\tEnter \"Y\" to delete this node, any other to keep: ";
+        cin >> charDeleteNode;
+        if(charDeleteNode == 'y' || charDeleteNode == 'Y')
+        {
+            cout << "\t\tDeleting: " << current << endl;
+            if(current == head){
+                head = head->nextNode;
+                delete current;
+                current = head;
+            }
+            else if(current == tail){
+                previous->nextNode = NULL;
+                delete current;
+                current = NULL;
+            }
+            else{
+                previous->nextNode = current->nextNode;
+                delete current;
+                current = previous->nextNode;
+            }
+
+        }
+        else{
+            previous = current;
+            current = current->nextNode;
+
+        }
+
+    };
+
+
+    current = head;
     while(current != NULL){
         cout << "Current: " << current << endl;
         cout << "\tData: " << current->intData << endl;
         cout << "\tNext Node: " << current->nextNode << endl;
         current = current->nextNode;
+    }
 
-    };
 
     return 0;
 }
